6b1f3bc63c6bfcec74cf35e3e02c407693281a04
NdikumNdifor <u0113322020@byui.edu>
2023-04-22 17:16:17 +0100

    C# Prep4 and Prep5 completed version

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        
+        while (number != 0)
+        {
+            Console.Write("Please enter a number: ");
+            string enteredNumber = Console.ReadLine();
+            number = int.Parse(enteredNumber);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }      
+        }
+
+        // Computing the sum of all the elements in the list.
+        int sum = 0;
+        foreach (int element in numbers)
+        {
+            sum += element;
+        }
+        Console.WriteLine();
+        Console.Write($"The sum is: {sum}");
+
+        // Calculating the average number.
+        float average = ((float)sum)/numbers.Count;
+        Console.WriteLine($"The average number is: {average}");
+
+        // Looking for the maximum number entered in the list
+        int max = -100000;
+        foreach (int numb in numbers)
+        if (numb > max)
+        {
+            max = numb;
+        }
+        Console.WriteLine($"The maximum nuber in the list is: {max}");
+        Console.WriteLine();
+
+        
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // A call to writen functions at the main.
+        DisplayWelcome();
+        string EnteredName = PromtUserName();
+        int EnteredNumber = PromtUserNumber();
+        int computeSquare = SquareNumber(EnteredNumber);
+        DisplayResult(EnteredName, computeSquare);
+        
+
+        // A function with that displays a welcome message and takes no parameter.
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the programm");
+        }
+
+        // Ask for the user's name and returns a string.
+        static string PromtUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        // Ask for the user's favorite number and returns an interger.
+        static int PromtUserNumber()
+        {
+            Console.Write("Eenter your favorite number: ");
+            string response = Console.ReadLine();
+            int number = int.Parse(response);
+            return number;
+        }
+
+        // Takes an interger as parameter,computes the squre of that interger and returns it.
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        // Takes a string and interger as parameters and display a result.
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"Your name is {name} and the square of your number is {square} ");
+        }
+        
     }
 }
